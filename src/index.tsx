f845c4c6074be4d61049100bb6b0314ec90e4bf0
Nikolay Vereshaghin <nikola-verr@outlook.com>
2021-08-20 17:45:29 +0300

    fix: font

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./fonts/SFUIText/5ad142d0e49ca868b1530c30a47c625f.eot";
+import "./fonts/SFUIText/5ad142d0e49ca868b1530c30a47c625f.ttf";
+import "./fonts/SFUIText/5ad142d0e49ca868b1530c30a47c625f.woff";
+import "./fonts/SFUIText/5ad142d0e49ca868b1530c30a47c625f.woff2";
+import "./fonts/SFUIText/5ad142d0e49ca868b1530c30a47c625fiefix.eot";
 import "./index.scss";
 
 ReactDOM.render(
